bf0a10667b854aec80d0cf55c793e5125d09d47b
easyonaaditya <--global>
2024-12-02 19:47:51 +0530

    added the lexer

---
+++ lexer/tokens.h
@@ -0,0 +1,34 @@
+#define ELSE 1
+#define SEMICOLON 2
+#define FOR 3
+#define INTERGER_LITERAL 4
+#define IDENTIFIER 5
+#define AND_LITERAL 6
+#define CLASS 7
+#define DO 8
+#define FALSE 10
+#define FOR 11
+#define IF 12
+#define TRUE 13
+#define WHILE 14
+#define ADD 15
+#define SUB 16
+#define MUL 17
+#define DIV 18
+#define OPEN_PARAENTHESES 19  // (
+#define CLOSE_PARAENTHESES 20 // )
+#define OPEN_BRACKET 21       // [
+#define CLOSE_BRACKET 22      // ]
+#define OPEN_BRACES 23        // {
+#define CLOSE_BRACES 24       // }
+#define PERIOD 25
+#define COMMA 26
+#define EQUALS 27
+#define NOT_EQUALS 28
+#define GREATER_LESSER 29
+#define GREATER_EQUALS 30
+#define LESSER_EQUALS 31
+#define ASSIGN 32
+#define AND 33
+#define OR 34
+#define NOT 35
